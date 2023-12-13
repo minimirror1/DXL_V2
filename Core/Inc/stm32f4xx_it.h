a704b3d36b8d0d24cf88ffe03f65f34c6f7af6c4
minimirror1 <minimirror1@gmail.com>
2023-12-13 22:04:02 +0900

    uart1 활성

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -55,6 +55,7 @@ void DebugMon_Handler(void);
 void CAN1_TX_IRQHandler(void);
 void CAN1_RX0_IRQHandler(void);
 void TIM4_IRQHandler(void);
+void USART1_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
